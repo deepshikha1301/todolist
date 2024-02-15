f1c3c434bf08c6cf2e4f539764c0ea687ff0202e
deepshikha <deepshikha2701@gmail.com>
2024-02-15 12:13:04 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  taskForm!:FormGroup;
   title = 'todolist';
+  taskList:string[] = [];
+
+  constructor(private formBuilder:FormBuilder){}
+
+  ngOnInit(): void{
+    this.taskForm = this.formBuilder.group({
+      taskName: ['','']
+    });
+  }
+
+  addTask(){
+    this.taskList.push(this.f['taskName'].value); 
+    console.log(this.taskList);
+  }
+
+  get f(){
+    return this.taskForm.controls;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input'
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
